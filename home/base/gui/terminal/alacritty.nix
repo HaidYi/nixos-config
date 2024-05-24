3999a4e77e785bdd61da7462f4ae3bd03d1ce560
HaidYi <haidyi@cs.unc.edu>
2024-05-23 20:49:02 -0400

    add config for alacritty

--- home/base/gui/terminal/alacritty.nix
+++ home/base/gui/terminal/alacritty.nix
@@ -20,37 +20,47 @@
 
       window = {
         padding = {
-	  x = 5;
-	  y = 5;
-	};
-	decorations = "full";
-	opacity = 0.95;
-	dynamic_title = true;
-	history = 10000;
-	multiplier = 3;
+    	    x = 5;
+	        y = 5;
+        };
+        decorations = "full";
+        opacity = 0.95;
+        dynamic_title = true;
+        history = 10000;
+        multiplier = 3;
       };
 
       font = {
         size = 13;
-	normal = {
-	  family = "JetBrains Mono Nerd Font";
-	  style = "Regular";
-	};
-	bold = {
-	  family = "JetBrains Mono Nerd Font";
-	  style = "Bold";
-	};
-	italic = {
-	  family = "JetBrains Mono Nerd Font";
-	  style = "Italic";
-	};
-	bold_italic = {
-	  family = "JetBrains Mono Nerd Font";
-	  style = "Bold Italic";
-	};
+        normal = {
+          family = "JetBrains Mono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrains Mono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrains Mono Nerd Font";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "JetBrains Mono Nerd Font";
+          style = "Bold Italic";
+        };
+      };
+
+      live_config_reload = true;
+
+      shell = {
+        program: "${pkgs.zsh}/bin/zsh";
+        args: [
+          "--login"
+        ];
       };
     };
   };
 
+  # config the alacritty's theme
   xdg.configFile."alacritty/color-scheme.toml".source = "./themes/alacritty-mocha.toml";
 }
