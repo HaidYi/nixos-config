ef64cbb6a711a55ba7377e87c2eeed13b3b935fc
HaidYi <haidyi@cs.unc.edu>
2024-05-23 20:35:39 -0400

    add zsh shell config

---
+++ home/base/core/shell.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "docker-compose" "docker" ];
+      theme = "dst";
+    };
+    initExtra = ''
+      bindkey '^f' autosuggest-accept
+    '';
+  };
+}
