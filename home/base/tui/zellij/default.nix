7fdbc201cb98341d9d224257007fd025f3aa8879
haidyi <haidyi@cs.unc.edu>
2024-08-08 18:24:26 -0400

    add multiplexer zellij

---
+++ home/base/tui/zellij/default.nix
@@ -0,0 +1,13 @@
+let
+  shellAliases = {
+    "zj" = "zellij";
+  }
+in {
+  programs.zellij = {
+    enable = true;
+    enableZshIntegration = true;
+  }
+
+  # only works in bash/zsh, instead of nushell
+  home.shellAliases = shellAliases;
+}
