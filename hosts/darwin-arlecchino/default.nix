e415e20ee6e36eb5248302a40da671669541a715
haidyi <haidyi@cs.unc.edu>
2024-08-08 18:24:43 -0400

    add arlecchino host

---
+++ hosts/darwin-arlecchino/default.nix
@@ -0,0 +1,13 @@
+_:
+#######################################
+#
+# Arlecchino - Mac Pro 2019, for personal use
+#
+#######################################
+let
+  hostname = "arlecchino"
+in {
+  networking.hostname = hostname;
+  networking.computerName = hostname;
+  system.defaults.smb.NetBIOSName = hostname;
+}
