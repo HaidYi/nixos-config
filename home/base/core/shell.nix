9e8a7efdea779e17fcdf8d322006a0eca0b6f14d
haidyi <haidyi@cs.unc.edu>
2024-08-17 17:32:13 -0400

    add catppuccin zsh hightlighting plugin

--- home/base/core/shell.nix
+++ home/base/core/shell.nix
@@ -10,5 +10,19 @@
     initExtra = ''
       bindkey '^f' autosuggest-accept
     '';
+
+    plugins = [
+      {
+        # will source zsh-syntax-highlighting-catppuccin-mocha
+        name = "zsh-syntax-highlighting-catppuccin-mocha";
+        file = "themes/catppuccin_mocha-zsh-syntax-highlighting.zsh";
+        src = pkgs.fetchFromGithub {
+          owner = "catppuccin";
+          repo = "zsh-syntax-highlighting";
+          rev = "ad3dcb5699ba36304b7b5c95d1f436f902e91f57";
+          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+      };
+    ];
   };
 }
