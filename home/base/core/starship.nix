ceaf341fd1168c7d0aa9a3beed4d79cb692a51f1
haidyi <haidyi@cs.unc.edu>
2024-08-17 18:37:19 -0400

    change prompt symbol

--- home/base/core/starship.nix
+++ home/base/core/starship.nix
@@ -19,8 +19,8 @@ in
       {
         add_newline = true;
         character = {
-          success_symbol = "[›](bold green)";
-          error_symbol = "[›](bold red)";
+          success_symbol = "[❯](bold green)";
+          error_symbol = "[❯](bold red)";
         };
 
         hostname = {
