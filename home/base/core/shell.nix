5ddd8c8d74f76f18c810c84631577ad446d8d996
haidyi <haidyi@cs.unc.edu>
2024-08-17 18:26:34 -0400

    add correct hash code

--- home/base/core/shell.nix
+++ home/base/core/shell.nix
@@ -16,13 +16,13 @@
         # will source zsh-syntax-highlighting-catppuccin-mocha
         name = "zsh-syntax-highlighting-catppuccin-mocha";
         file = "themes/catppuccin_mocha-zsh-syntax-highlighting.zsh";
-        src = pkgs.fetchFromGithub {
+        src = pkgs.fetchFromGitHub {
           owner = "catppuccin";
           repo = "zsh-syntax-highlighting";
           rev = "ad3dcb5699ba36304b7b5c95d1f436f902e91f57";
-          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          hash = "sha256-lkbuNFHXPqVW5EeJAeGipv7fdzB/EEb8T4n5TcVxSSA=";
         };
-      };
+      }
     ];
   };
 }
