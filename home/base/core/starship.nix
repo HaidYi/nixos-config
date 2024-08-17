109ce819a7b410c0cdb87f80a0d26a268d559431
haidyi <haidyi@cs.unc.edu>
2024-08-17 17:32:21 -0400

    add more starship config

--- home/base/core/starship.nix
+++ home/base/core/starship.nix
@@ -23,58 +23,74 @@ in
           error_symbol = "[›](bold red)";
         };
 
+        hostname = {
+          format = "on [$hostname]($style)";
+          style = "bold yellow";
+        };
+
+        git_branch = {
+          format = "[$symbol$branch]($style)";
+          symbol = " ";
+          style = "bold mauve";
+        };
+
+        git_commit = {
+          commit_hash_length = 6;
+        };
+
         os = {
           disabled = false;
+          format = "$symbol ";
         };
 
         os.symbols = {
           AIX = "➿ ";
-          Alpaquita = "🔔 ";
-          AlmaLinux = "💠 ";
-          Alpine = "🏔️ ";
-          Amazon = "🙂 ";
-          Android = "🤖 ";
-          Arch = "🎗️ ";
-          Artix = "🎗️ ";
-          CentOS = "💠 ";
-          Debian = "🌀 ";
-          DragonFly = "🐉 ";
-          Emscripten = "🔗 ";
-          EndeavourOS = "🚀 ";
-          Fedora = "🎩 ";
-          FreeBSD = "😈 ";
-          Garuda = "🦅 ";
-          Gentoo = "🗜️ ";
-          HardenedBSD = "🛡️ ";
-          Illumos = "🐦 ";
-          Kali = "🐉 ";
-          Linux = "🐧 ";
-          Mabox = "📦 ";
-          Macos = "🍎 ";
-          Manjaro = "🥭 ";
-          Mariner = "🌊 ";
-          MidnightBSD = "🌘 ";
-          Mint = "🌿 ";
-          NetBSD = "🚩 ";
-          NixOS = "❄️ ";
-          OpenBSD = "🐡 ";
-          OpenCloudOS = "☁️ ";
-          openEuler = "🦉 ";
-          openSUSE = "🦎 ";
-          OracleLinux = "🦴 ";
-          Pop = "🍭 ";
-          Raspbian = "🍓 ";
-          Redhat = "🎩 ";
-          RedHatEnterprise = "🎩 ";
-          RockyLinux = "💠 ";
-          Redox = "🧪 ";
-          Solus = "⛵ ";
-          SUSE = "🦎 ";
-          Ubuntu = "🎯 ";
-          Ultramarine = "🔷 ";
-          Unknown = "❓ ";
-          Void = "  ";
-          Windows = "🪟 ";
+          Alpaquita = "🔔";
+          AlmaLinux = "💠";
+          Alpine = "🏔️";
+          Amazon = "🙂";
+          Android = "🤖";
+          Arch = "🎗️";
+          Artix = "🎗️";
+          CentOS = "💠";
+          Debian = "🌀";
+          DragonFly = "🐉";
+          Emscripten = "🔗";
+          EndeavourOS = "🚀";
+          Fedora = "🎩";
+          FreeBSD = "😈";
+          Garuda = "🦅";
+          Gentoo = "🗜️";
+          HardenedBSD = "🛡️";
+          Illumos = "🐦";
+          Kali = "🐉";
+          Linux = "🐧";
+          Mabox = "📦";
+          Macos = "🍎";
+          Manjaro = "🥭";
+          Mariner = "🌊";
+          MidnightBSD = "🌘";
+          Mint = "🌿";
+          NetBSD = "🚩";
+          NixOS = "❄️";
+          OpenBSD = "🐡";
+          OpenCloudOS = "☁️";
+          openEuler = "🦉";
+          openSUSE = "🦎";
+          OracleLinux = "🦴";
+          Pop = "🍭";
+          Raspbian = "🍓";
+          Redhat = "🎩";
+          RedHatEnterprise = "🎩";
+          RockyLinux = "💠";
+          Redox = "🧪";
+          Solus = "⛵";
+          SUSE = "🦎";
+          Ubuntu = "🎯";
+          Ultramarine = "🔷";
+          Unknown = "❓";
+          Void = " ";
+          Windows = "🪟";
         };
 
         palette = "catppuccin_mocha";
