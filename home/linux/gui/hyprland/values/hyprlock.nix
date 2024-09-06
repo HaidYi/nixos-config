15a354bdcccd29869870dc7075036e40a8fa44e1
haidyi <haidyi@cs.unc.edu>
2024-09-05 22:12:13 -0500

    add hyprlock config

---
+++ home/linux/gui/hyprland/values/hyprlock.nix
@@ -0,0 +1,96 @@
+{
+  pkgs,
+  ...
+}: {
+
+  programs.hyprlock = {
+    enable = true;
+
+    settings = {
+      general = {
+        disable_loading_bar = true;
+        hide_cursor = true;
+      };
+    };
+
+    extraConfig = ''
+      source $XDG_CONFIG_HOME/hypr/hyprlock/mocha.conf
+
+      $accent = $mauve
+      $accentAlpha = $mauveAlpha
+      $font = JetBrainsMono Nerd Font
+
+      # background
+      background {
+        monitor =
+        path = $XDG_CONFIG_HOME/hypr/hyprlock/background.png
+        blur_passes = 0
+        color = $base
+      }
+
+      # layout
+      label {
+        monitor =
+        text = Layout: $LAYOUT
+        color = $text
+        font_size = 25
+        font_family = $font
+        position = 30, -30
+        halign = left
+        valign = top
+      }
+
+      # time
+      label {
+        monitor = 
+        text = $TIME
+        color = $text
+        font_size = 90
+        font_family = $font
+        position = -30, 0
+        halign = right
+        valign = top
+      }
+
+      # date
+      label {
+        monitor =
+        text = cmd[update:43200000] date +"%A, %d %B %Y"
+        color = $text
+        font_size = 25
+        font_family = $font
+        position = -30, -150
+        halign = right
+        valign = top
+      }
+
+      # TODO: user avatar
+
+      # input_field
+      input-field {
+        monitor =
+        size = 300, 60
+        outline_thickness = 4
+        dots_size = 0.2
+        dots_spacing = 0.2
+        dots_center = true
+        outer_color = $accent
+        inner_color = $surface0
+        font_color = $text
+        fade_on_empty = false
+        placeholder_text = <span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>
+        hide_input = false
+        check_color = $accent
+        fail_color = $red
+        fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
+        capslock_color = $yellow
+        position = 0, -47
+        halign = center
+        valign = center
+      }
+    '';
+  };
+
+  xdg.configFile."hypr/hypylock/mocha.conf".source = ../conf/themes/mocha.conf;
+  xdg.configFile."hypr/hypylock/background.png".source = ../conf/hyprlock/nix-girl.png;
+}
