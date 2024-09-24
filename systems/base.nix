0377f2c7793ba8ab30831d7878779f65ddd6201b
Haidong Yi <haidong.yi@stjude.org>
2024-09-23 21:46:39 -0500

    add trusted user

--- systems/base.nix
+++ systems/base.nix
@@ -53,6 +53,11 @@
   nix.settings = {
     # enable flakes globally
     experimental-features = ["nix-command" "flakes"];
+
+    # given the users in this list the right to specify additional substituters via:
+    #    1. `nixConfig.substituers` in `flake.nix`
+    #    2. command line args `--options substituers http://xxx`
+    trusted-users = [myvars.username];
   };
 
 
@@ -63,5 +68,6 @@
   # make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
   # discard all the default paths, and only use the one from this flake.
   nix.nixPath = lib.mkForce ["/etc/nix/inputs"];
+  # https://github.com/NixOS/nix/issues/9574
   nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
 }
