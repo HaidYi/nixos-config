cac2cde35a97407364c8e892547e7585bcf18603
Haidong Yi <haidong.yi@stjude.org>
2024-09-23 21:26:23 -0500

    add terminfo config

--- systems/nixos/common/packages.nix
+++ systems/nixos/common/packages.nix
@@ -33,4 +33,8 @@
 
   # replace default editor with neovim
   environment.variables.EDITOR = "nvim";
+
+  # Add terminfo database of all known terminals to the system profile.
+  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/config/terminfo.nix
+  environment.enableAllTerminfo = true;
 }
