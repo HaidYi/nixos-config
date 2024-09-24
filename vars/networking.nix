ba81a26920dfb0d754a2191745ad846c618642c0
Haidong Yi <haidong.yi@stjude.org>
2024-09-23 22:44:00 -0500

    add defaultGateway

--- vars/networking.nix
+++ vars/networking.nix
@@ -1,6 +1,7 @@
 {lib}: rec {
   mainGateway = "192.168.1.1"; # main router
-  
+  defaultGateway = "192.168.1.1"; # default gateway: main router
+
   nameservers = [
     "192.168.1.1" # router's address for redirecting DNS request
     "8.8.8.8"  # google's public DNS Ip address
