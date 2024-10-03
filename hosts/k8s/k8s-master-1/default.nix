07735830196e5c7b51119c929bdf5a8e4ee86195
HaidYi <haidyi@cs.unc.edu>
2024-10-02 22:40:31 -0500

    add the first node for testing network

---
+++ hosts/k8s/k8s-master-1/default.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  pkgs,
+  myvars,
+  mylib,
+  ...
+}: let
+
+  hostName = "k8s-master-1"; # define the hostname
+
+  coreModule = mylib.genK8sCoreModule {
+    inherit pkgs hostName;
+    inherit (myvars) networking;
+  };
+
+in {
+  imports =
+    (mylib.scanPaths ./.)
+  ++ [
+    # ../disko-config.nix
+    coreModule
+  ];
+}
---
+++ outputs/x86_64-linux/src/k8s-master-1.nix
@@ -0,0 +1,36 @@
+{
+  # NOTE: the args not used in this file CAN NOT be removed!
+  # because haumea pass argument lazily,
+  # and these arguments are used in the functions like `mylib.nixosSystem`, `mylib.colmenaSystem`, etc.
+  inputs,
+  lib,
+  myvars,
+  mylib,
+  system,
+  genSpecialArgs,
+  ...
+} @ args: let
+  # 虞美人
+  name = "k8s-master-1";
+
+  modules = {
+    nixos-modules =
+      (map mylib.relativeToRoot [
+        # common
+        "systems/nixos/server/server.nix"
+        # host specific
+        "hostds/k8s/${name}"
+      ]);
+
+    home-modules = map mylib.relativeToRoot [
+      "home/linux/core.nix"
+    ];
+  };
+
+  systemArgs = modules // args;
+
+in {
+  nixConfigurations.${name} = mylib.nixosSystem systemArgs;
+
+  packages.${name} = inputs.self.nixosConfigurations."${name}".config.formats.qcow2;
+}
