0683920994b7f219c14a72454d0a946193616f42
HaidYi <haidyi@cs.unc.edu>
2024-10-08 22:26:42 -0500

    add k8s worker hosts

---
+++ outputs/x86_64-linux/src/k8s-worker-2.nix
@@ -0,0 +1,32 @@
+{
+  # NOTE: the args not used in this file CAN NOT be removed!
+  # because haumea pass argument lazily,
+  # and these arguments are used in the functions like `mylib.nixosSystem`, `mylib.colmenaSystem`, etc.
+  inputs,
+  lib,
+  myvars,
+  mylib,
+  system,
+  genSpecialArgs,
+  ...
+} @ args: let
+  # 如梦令
+  name = "k8s-worker-2";
+
+  modules = {
+    nixos-modules =
+      (map mylib.relativeToRoot [
+        # common
+        "systems/nixos/server/server.nix"
+        # host specific
+        "hosts/k8s/${name}"
+      ]);
+  };
+
+  systemArgs = modules // args;
+
+in {
+  nixosConfigurations.${name} = mylib.nixosSystem systemArgs;
+
+  packages.${name} = inputs.self.nixosConfigurations.${name}.config.formats.qcow;
+}
