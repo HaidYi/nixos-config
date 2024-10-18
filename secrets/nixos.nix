e2a4b58cba15d4b42ab800179553c5b9e448d6f9
HaidYi <haidyi@cs.unc.edu>
2024-10-17 22:10:40 -0500

    add secrets nixos

---
+++ secrets/nixos.nix
@@ -0,0 +1,76 @@
+{
+  lib,
+  config,
+  pkgs,
+  agenix,
+  mysecrets,
+  myvars,
+  ...
+}: let
+  noaccess = {
+    mode = "0000";
+    owner = "root";
+  };
+
+  high_security = {
+    mode = "0500";
+    owner = "root";
+  };
+
+  user_readable = {
+    mode = "0500";
+    owner = myvars.username;
+  };
+in {
+
+  imports = [
+    agenix.nixosModules.default
+  ];
+
+  environment.systemPackages = [
+    agenix.packages."${pkgs.system}".default
+  ];
+
+  # set the identity key for decryption
+  age.identityPaths = [
+    "/persistent/etc/ssh/ssh_recovery_key"
+  ];
+
+  # secret files
+  age.secrets = {
+
+    # -----------------------------
+    # only root can read this file
+    # -----------------------------
+    "nixos-luks-keyfile" = {
+      file = "${mysecrets}/nixos-luks-keyfile.age";
+    }
+    // high_security;
+
+    # -----------------------------
+    # user can read this file
+    # -----------------------------
+    "k8s-ca.key" = {
+      file = "${mysecrets}/k8s-ca.key.age";
+    }
+    // user_readable;
+
+  };
+
+  # place secrets in /etc/
+  environment.etc = {
+
+    "agenix/nixos-luks-keyfile" = {
+      source = config.age.secrets."nixos-luks-keyfile".path;
+      mode = "0400";
+      user = root;
+    };
+
+    "agenix/k8s-ca.key" = {
+      source = config.age.secrets."k8s-ca.key".path;
+      mode = "0400";
+      user = myvars.username;
+    };
+  };
+
+}
