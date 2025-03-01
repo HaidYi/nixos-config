f116281a4408420d4c8c042a3af9b0a7060e3949
HaidYi <haidyi@cs.unc.edu>
2025-02-28 21:57:59 -0600

    add anyrun as input

--- flake.nix
+++ flake.nix
@@ -48,6 +48,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # community wayland nixpkgs
+    # nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+    # anyrun - a wayland launcher
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # generate iso/qcow2/docker/... image from nixos configuration
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
