3af113728bed5d6602f5143c6e002f96c6401633
HaidYi <haidyi@cs.unc.edu>
2025-02-28 22:26:42 -0600

    change k8s to k3s

--- hosts/honkai-gpt/k8s.nix
+++ hosts/honkai-gpt/k8s.nix
@@ -3,42 +3,50 @@ let
   kubeMasterIP = "192.168.1.21";
   kubeMasterHostname = "gpt";
   kubeMasterAPIServerPort = 6443;
+  lib = pkgs.lib;
 in
 {
   networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
 
   environment.systemPackages = with pkgs; [
-    kompose
+    k9s
     kubectl
-    kubernetes
   ];
 
-  services.kubernetes = {
-    roles = ["master" "node"];
-    masterAddress = kubeMasterHostname;
-    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
-    easyCerts = true;
-    apiserver = {
-      securePort = kubeMasterAPIServerPort;
-      advertiseAddress = kubeMasterIP;
-    };
+  virtualisation.containerd.enable = true; 
 
-    controllerManager = {
-      securePort = 10257;
-    };
+  services.k3s = rec {
+    enable = true;
+    package = pkgs.k3s;
 
-    scheduler = {
-      port = 10259;
-    };
+    clusterInit = true;
+    serverAddr = 
+      if clusterInit
+      then ""
+      else "https://${kubeMasterHostname}:6443";
 
-    addons.dns.enable = true;
-
-    # needed if you use swap
-    kubelet.extraOpts = "--fail-swap-on=false";
+    role = "server";
+    extraFlags = let
+      flagList = [
+        "--write-kubeconfig-mode=644"
+        "--kube-apiserver-arg='--allow-privileged=true'" # required by kubevirt
+        "--data-dir /var/lib/rancher/k3s"
+        "--etcd-expose-metrics=true"
+        "--disable=servicelb" # we use kube-vip instead
+        "--disable-network-policy"
+        "--tls-san=${kubeMasterHostname}"
+        "--flannel-backend=none" # disable flannel as we use calico
+      ];
+    in
+      lib.concatStringsSep " " flagList;
   };
 
-  services.flannel.backend = {
-    Type = "host-gw";
-  };
+  systemd.tmpfiles.rules = [
+    "L+ /opt/cni/bin - - - - /var/lib/rancher/k3s/data/current/bin"
+    # If you have disabled flannel, you will have to create the directory via a tmpfiles rule
+    "d /var/lib/rancher/k3s/agent/etc/cni/net.d 0751 root root - -"
+    # Link the CNI config directory
+    "L+ /etc/cni/net.d - - - - /var/lib/rancher/k3s/agent/etc/cni/net.d"
+  ];
 
 }
